8626f09c1a7f9b8916e136cd6bf499ebe6908296
Jun <leej1025y@live.com>
2025-01-03 21:38:04 +1100

    fix: beats 88.91%

--- threeSum.js
+++ threeSum.js
@@ -37,92 +37,22 @@ var isValid = function(s) {
         }
     }
 
-    console.log(`temp.length: ${temp.length}, ${s.length % 2}`)
     if (temp.length === 0 && s.length % 2 != 0) {
         return false
     }
 
     return temp.length === 0
-
-
-
-
-    // const firstChar = s.charAt(0)
-    // const lastChar = s.charAt(s.length - 1)
-
-    // if (
-    //     (firstChar === "(" && lastChar != ")") ||
-    //     (firstChar === "[" && lastChar != "]") ||
-    //     (firstChar === "{" && lastChar != "}")
-    // ) {
-    //     return false
-    // }
-
-    // let roundBracketStartCount = 0
-    // let roundBracketEndCount = 0
-    // let curlyBracketStartCount = 0
-    // let curlyBracketEndCount = 0
-    // let squareBracketStartCount = 0
-    // let squareBracketEndCount = 0
-
-    // let previousChar = ""
-
-    // for (let i = 0; i < s.length; i++) {
-    //     const currentChar = s.charAt(i)
-
-    //     if (currentChar === "(") {
-    //         roundBracketStartCount += 1
-    //     } else if (currentChar === ")") {
-    //         if (previousChar === "{" || previousChar === "[") {
-    //             return false
-    //         }
-
-    //         roundBracketEndCount += 1
-    //     } else if (currentChar === "{") {
-    //         curlyBracketStartCount += 1
-    //     } else if (currentChar === "}") {
-    //         if (previousChar === "(" || previousChar === "[") {
-    //             return false
-    //         }
-
-    //         curlyBracketEndCount += 1
-    //     } else if (currentChar === "[") {
-    //         squareBracketStartCount += 1
-    //         canCloseSquareBracket = true
-    //     } else if (currentChar === "]") {
-    //         if (previousChar === "(" || previousChar === "{") {
-    //             return false
-    //         }
-
-    //         squareBracketEndCount += 1
-    //     }
-
-    //     if (
-    //         squareBracketEndCount > squareBracketStartCount ||
-    //         curlyBracketEndCount > curlyBracketStartCount ||
-    //         roundBracketEndCount > roundBracketStartCount
-    //     ) {
-    //         return false
-    //     }
-
-    //     previousChar = currentChar
-    // }
-
-    // const isValidString = roundBracketStartCount === roundBracketEndCount &&
-    //     curlyBracketStartCount === curlyBracketEndCount &&
-    //     squareBracketStartCount === squareBracketEndCount
-    // return isValidString
 };
 
-console.log(`() : ${isValid("()") === true}`)
-console.log(`()[]{} : ${isValid("()[]{}") === true}`)
-console.log(`(] : ${isValid("(]") === false}`)
-console.log(`([]) : ${isValid("([])") === true}`)
-console.log(`([)] : ${isValid("([)]") === false}`)
-console.log(`] : ${isValid("]") === false}`)
-console.log(`)(){} : ${isValid(")(){}") === false}`)
-console.log(`[]) : ${isValid("[])") === false}`)
-console.log(`(([]){}) : ${isValid("(([]){})") === true}`)
-console.log(`(){}}{ : ${isValid("(){}}{") === false}`)
-console.log(`[([]]) : ${isValid("[([]])") === false}`)
-console.log(`([}}]) : ${isValid("([}}])") === false}`)
+// console.log(`() : ${isValid("()") === true}`)
+// console.log(`()[]{} : ${isValid("()[]{}") === true}`)
+// console.log(`(] : ${isValid("(]") === false}`)
+// console.log(`([]) : ${isValid("([])") === true}`)
+// console.log(`([)] : ${isValid("([)]") === false}`)
+// console.log(`] : ${isValid("]") === false}`)
+// console.log(`)(){} : ${isValid(")(){}") === false}`)
+// console.log(`[]) : ${isValid("[])") === false}`)
+// console.log(`(([]){}) : ${isValid("(([]){})") === true}`)
+// console.log(`(){}}{ : ${isValid("(){}}{") === false}`)
+// console.log(`[([]]) : ${isValid("[([]])") === false}`)
+// console.log(`([}}]) : ${isValid("([}}])") === false}`)
