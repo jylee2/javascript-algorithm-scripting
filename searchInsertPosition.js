91fe157e9fad7fb014006c91df46627e0f4cf565
Jun <leej1025y@live.com>
2025-01-05 23:05:50 +1100

    fix: 64 / 65 testcases passed

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -4,25 +4,33 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    if (target < nums[0]) {
+    if (nums[0] === target) {
         return 0
     }
-    if (target > nums[nums.length - 1]) {
+    if (nums[0] > target) {
+        return 0
+    }
+    if (nums[nums.length - 1] < target) {
         return nums.length
+    }
+    if (nums[nums.length - 1] === target) {
+        return nums.length - 1
     }
 
     let leftIndex = Math.floor(nums.length / 2)
     let rightIndex = Math.floor(nums.length / 2)
 
     for (let i = 0; i < nums.length; i++) {
-        // console.log(`leftIndex: ${leftIndex}, rightIndex: ${rightIndex}`)
-
         if (nums[leftIndex] === target) {
             return leftIndex
         }
 
         if (nums[leftIndex - 1] < target && target < nums[leftIndex]) {
             return leftIndex
+        }
+
+        if (nums[leftIndex - 1] === target) {
+            return leftIndex - 1
         }
 
         if (nums[rightIndex] === target) {
@@ -46,3 +54,5 @@ console.log(`[1,3,5,6], 7 : ${searchInsert([1,3,5,6], 7) === 4}, ${searchInsert(
 console.log(`[1,3,5,6], 0 : ${searchInsert([1,3,5,6], 0) === 0}, ${searchInsert([1,3,5,6], 0)}`)
 console.log(`[1], 1 : ${searchInsert([1], 1) === 0}, ${searchInsert([1], 1)}`)
 console.log(`[1,3,5], 4 : ${searchInsert([1,3,5], 4) === 2}, ${searchInsert([1,3,5], 4)}`)
+console.log(`[1,4,6,7,8,9], 6 : ${searchInsert([1,4,6,7,8,9], 6) === 2}, ${searchInsert([1,4,6,7,8,9], 6)}`)
+console.log(`[1,2,4,6,8,9,10], 10 : ${searchInsert([1,2,4,6,8,9,10], 10) === 6}, ${searchInsert([1,2,4,6,8,9,10], 10)}`)
